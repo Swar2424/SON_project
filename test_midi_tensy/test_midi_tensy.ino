236d35edead9e9d06eb22e1a0794adf17de9d479
Swar2424 <154347168+Swar2424@users.noreply.github.com>
2024-02-14 09:07:46 +0100

    added 1 channel + better code with cases and stuff

--- test_midi_tensy/test_midi_tensy.ino
+++ test_midi_tensy/test_midi_tensy.ino
@@ -9,11 +9,12 @@ AudioConnection patchCord0(myDsp,0,out,0);
 AudioConnection patchCord1(myDsp,0,out,1);
 
 int ledPin = 13;
-int canaux[3] = {0, 0, 0};
+int canaux[4] = {0, 0, 0, 0};
+int len = sizeof(canaux) / sizeof(canaux[0]);
 
 void OnNoteOn(byte channel, byte note, byte velocity) {
   bool placed = false;
-  for (int i = 0; i<3 && placed == false; i++){
+  for (int i = 0; i<len && placed == false; i++){
     if (canaux[i] == 0) {
       canaux[i] = note;
       myDsp.setFreq(i, pow(2,(note-69)/12.0)*440.0);
@@ -21,33 +22,26 @@ void OnNoteOn(byte channel, byte note, byte velocity) {
       placed = true;
     }
   }
-  Serial.print(canaux[0]);  
-  Serial.print(canaux[1]);  
-  Serial.println(canaux[2]);  
 }
 
 void OnNoteOff(byte channel, byte note, byte velocity) {
-  for (int i = 0; i<3 ; i++){
+  for (int i = 0; i<len ; i++){
     if (note == canaux[i]) {
       myDsp.setMute(i, 0);
       canaux[i] = 0;
     }
   }
-  Serial.print(canaux[0]);  
-  Serial.print(canaux[1]);
-  Serial.println(canaux[2]); 
 }
 
 void setup() {
   AudioMemory(2);
   audioShield.enable();
   audioShield.volume(0.2);
-  myDsp.setGaine(0, 0.2);
-  myDsp.setGaine(1, 0.2);
-  myDsp.setGaine(2, 0.2);
-  myDsp.setMute(0, 0);
-  myDsp.setMute(1, 0);
-  myDsp.setMute(2, 0);
+
+  for (int i=0 ; i<len ; i++) {
+     myDsp.setMute(i, 0);
+  }
+  myDsp.setGaine(0.2);
   pinMode(ledPin, OUTPUT);
   usbMIDI.setHandleNoteOff(OnNoteOff);
   usbMIDI.setHandleNoteOn(OnNoteOn) ;
